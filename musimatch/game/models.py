d4f64df4e67cb3fb99c4f873a813880d0bb25338
Austin <WANG.AUSTINC@GMAIL.COM>
2024-04-09 22:20:19 -0400

    game model created, some minor changes

--- musimatch/game/models.py
+++ musimatch/game/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    artist = models.CharField(max_length=25)
+    song = models.CharField(max_length=50)
+    timeperiod = models.DateField()
+    date = models.DateTimeField(auto_now_add=True)
+
--- musimatch/game/views.py
+++ musimatch/game/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def join_game(request):
-    return render(request, 'join_game.html')
+    context = {}
+    list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    context['things'] = list
+    return render(request, 'join_game.html', context)
 
