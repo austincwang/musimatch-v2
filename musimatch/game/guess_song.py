b1a0210bfac45987129e8fa932b41ea71b132939
cdtyu <chrisdtyu@gmail.com>
2024-04-13 15:21:36 -0400

    style changes

---
+++ musimatch/game/guess_song.py
@@ -0,0 +1,117 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import random
+import time
+from pygame import mixer
+import requests
+from io import BytesIO
+import nltk
+
+nltk.download('words')
+from nltk.corpus import words
+word_list = words.words()
+
+# Spotify API credentials
+client_id = '845df6cbf5ee42f4884ebd14605fd17c'
+client_secret = 'd59c601d020b4266bc2ab27a95520cb1'
+
+# Set up Spotify client credentials
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def download_song_preview(track_id):
+    # Get the track's preview URL
+    track_info = sp.track(track_id)
+    preview_url = track_info.get('preview_url')
+
+    if not preview_url:
+        print(f"Sorry, the audio preview is not available for the track {track_info['name']}")
+        return None
+
+    # Download the audio preview
+    try:
+        response = requests.get(preview_url)
+        audio_data = BytesIO(response.content)
+        return audio_data
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading audio preview: {e}")
+        return None
+
+def play_song_preview(track_id):
+    # Download the audio preview
+    audio_data = download_song_preview(track_id)
+
+    if audio_data is None:
+        return
+
+    # Initialize the mixer
+    mixer.init()
+    mixer.music.load(audio_data)
+    mixer.music.play()
+
+    # Wait for the song to finish playing
+    time.sleep(5) 
+
+    # Stop the song
+    mixer.music.stop()
+
+def play_game():
+    # # Get a list of popular tracks
+    # results = sp.playlist_tracks('37i9dQZEVXbMDoHDwVN2tF')  # This is the ID of Spotify's "Today's Top Hits" playlist
+    # tracks = results['items']
+
+    # # Select a random track from the playlist
+    # random_track = random.choice(tracks)
+    # track_name = random_track['track']['name']
+    # track_id = random_track['track']['id']
+
+
+    while True:
+        random_q = random.choice(word_list)
+        # Get a random list of tracks with audio previews
+        results = sp.search(q=random_q, type='track', limit=50)
+        tracks_with_previews = [track for track in results['tracks']['items'] if track.get('preview_url')]
+
+        if not tracks_with_previews:
+            print("No tracks with audio previews found. Exiting.")
+            exit()
+        
+        # Shuffle the list of tracks (can remove)
+        random.shuffle(tracks_with_previews)
+
+        # Select a random track from the list
+        random_track = random.choice(tracks_with_previews)
+        track_name = random_track['name']
+        track_id = random_track['id']
+
+        print("Guess the song!")
+        print(f"Song: {track_name}")
+        
+        # Play the song preview
+        play_song_preview(track_id)
+
+        # Get user's guess
+        user_guess = input("Your guess: ").strip()
+
+        # Check if the guess is correct
+        if user_guess.lower() == track_name.lower():
+            print(f"{track_name} is correct!")
+            break
+        else:
+            print(f"The correct answer was: {track_name}")
+            break
+
+# Main loop for the game
+while True:
+    play_game()
+
+    # Ask if the user wants to play again
+    while True:
+        play_again = input("Do you want to play again? (y/n): ").lower()
+        if play_again == 'n':
+            print("Thanks for playing!")
+            exit()  # Exiting the script if the user chooses not to play again
+        elif play_again == 'y':
+            break  # Break the inner loop and play again
+        else:
+            print("Invalid input. Please enter 'y' or 'n'.")
--- musimatch/game/views.py
+++ musimatch/game/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+import requests
+from io import BytesIO
+from pygame import mixer
+import time
+
 
 # Create your views here.
 def join_game(request):
@@ -7,3 +15,57 @@ def join_game(request):
     context['things'] = list
     return render(request, 'join_game.html', context)
 
+
+# ok i need to fix this part
+def index(request):
+    if request.method=='POST':
+        artist_uri = request.POST.get('uri')
+        spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=\
+                '845df6cbf5ee42f4884ebd14605fd17c',client_secret='d59c601d020b4266bc2ab27a95520cb1',))
+        results = spotify.artist_top_tracks(artist_uri)
+        final_result=results['tracks'][:10]
+        return render(request,'base.html',{"results":final_result})
+    else:
+# for track in results['tracks'][:10]:
+#     print('track    : ' + track['name'])
+#     print('audio    : ' + track['preview_url'])
+#     print('cover art: ' + track['album']['images'][0]['url'])
+#     print()
+      return render(request,'base.html',)
+
+
+# # Initialize Spotipy client credentials
+# client_id = '845df6cbf5ee42f4884ebd14605fd17c'
+# client_secret = 'd59c601d020b4266bc2ab27a95520cb1'
+# client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+# sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+# def play_song(request):
+#     track_id = 'your_track_id_here'  # You need to pass a valid track ID
+#     audio_data = download_song_preview(track_id)
+#     if audio_data:
+#         play_audio(audio_data)
+#         return HttpResponse("Song played successfully!")
+#     else:
+#         return HttpResponse("Error: Unable to play song.")
+
+# def download_song_preview(track_id):
+#     track_info = sp.track(track_id)
+#     preview_url = track_info.get('preview_url')
+#     if preview_url:
+#         try:
+#             response = requests.get(preview_url)
+#             audio_data = BytesIO(response.content)
+#             return audio_data
+#         except requests.exceptions.RequestException as e:
+#             print(f"Error downloading audio preview: {e}")
+#     else:
+#         print(f"Sorry, the audio preview is not available for the track {track_info['name']}")
+#         return None
+
+# def play_audio(audio_data):
+#     mixer.init()
+#     mixer.music.load(audio_data)
+#     mixer.music.play()
+#     time.sleep(5)
+#     mixer.music.stop()
