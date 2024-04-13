49c1d90d51e12c957c2c1164c037ef63a8cbdaf5
Austin <WANG.AUSTINC@GMAIL.COM>
2024-04-13 16:57:46 -0400

    added user login, register, logout and authentication

--- musimatch/game/urls.py
+++ musimatch/game/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'game'
+
 urlpatterns = [
-    path('join', views.join_game),
+    path('join/', views.join_game, name='join'),
 ]
--- musimatch/musimatch/settings.py
+++ musimatch/musimatch/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'game',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -75,13 +76,22 @@ WSGI_APPLICATION = 'musimatch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "RSVfcOgTwOeKeERTjybdWdriCanwfqKj",
+        "HOST": "roundhouse.proxy.rlwy.net",
+        "PORT": "29006",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
